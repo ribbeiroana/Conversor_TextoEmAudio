42ecb5f9700d6d034389357baa8bcb41eaceb212
Ana Paula <132491694+ribbeiroana@users.noreply.github.com>
2024-09-18 15:28:53 -0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,69 @@
+const selecaoVoz = document.querySelector('#selecao-voz');
+const entradaTexto = document.querySelector('#entrada-de-texto');
+const botaoOuvir = document.querySelector('#ouvir-btn');
+const botaoBaixarTexto = document.querySelector('#baixar-texto-btn');
+const uploaArquivo = document.querySelector('#upload-arquivo');
+
+const fala = new SpeechSynthesisUtterance()
+
+let vozesDisponiveis = [];
+
+const atualizarValores = () => {
+  vozesDisponiveis = window.speechSynthesis.getVoices()
+
+  fala.voice = vozesDisponiveis[0]
+
+
+  vozesDisponiveis.forEach((voz, index) => {
+    const opcao = document.createElement('option')
+    opcao.value = index;
+    opcao.textContent = voz.name;
+    selecaoVoz.appendChild(opcao);
+
+  });
+};
+
+window.speechSynthesis.onvoiceschanged = atualizarValores;
+
+selecaoVoz.addEventListener('change', () => {
+  fala.voice = vozesDisponiveis[selecaoVoz.value];
+})
+
+botaoOuvir.addEventListener('click', () => {
+  fala.text = entradaTexto.value
+
+  window.speechSynthesis.speak(fala)
+})
+
+botaoBaixarTexto.addEventListener('click', () => {
+  const text = entradaTexto.value
+
+  const blob = new Blob([text], { type: 'text/plain' });
+
+  const url = URL.createObjectURL(blob);
+
+  const a = document.createElement('a');
+
+  a.href = url 
+
+  a.download = 'texto.txt'
+
+  a.click()
+
+  URL.revokeObjectURL(url);
+
+});
+
+uploaArquivo.addEventListener('change', (e) => {
+  const arquivo = e.target.files[0]
+
+  if (arquivo) {
+    const leitor = new FileReader()
+
+    leitor.onload = (e) => {
+      entradaTexto.value = e.target.result;
+    };
+
+    leitor.readAsText(arquivo);
+  }
+})
